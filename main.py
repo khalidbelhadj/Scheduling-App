aa9751e14f0a99c5d776069767b0532e30257235
Bombearo <shekjaden@gmail.com>
2024-02-01 21:24:53 +0000

    Refactored for postgres support

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
+
+
+
+# Load dotenv
+from dotenv import load_dotenv
+import json
+import psycopg2
 from flask import Flask, request, jsonify, redirect, url_for, session, render_template
 from werkzeug.security import generate_password_hash, check_password_hash
-import json
+import os
+from dotenv import load_dotenv
+from configparser import ConfigParser
+import datetime
+load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = 'secret key'
 
-#Load dotenv
-from dotenv import load_dotenv
-import os
-load_dotenv()
 
-ADMIN_PASS = os.getenv("ADMIN_PASS")
 # Dummy user database
 dummy_users = [{
-        'name': 'admin',
-        'username': 'admin',
-        'password': generate_password_hash(ADMIN_PASS)
-    }]
-
+    'name': 'admin',
+    'username': 'admin',
+    'password': generate_password_hash('admin')
+}]
 
 days = list(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"])
 dates = [
@@ -35,7 +40,7 @@ dates = [
     "2024-04-01"
 ]
 
-titles= [
+titles = [
     "Week 1",
     "Week 2",
     "Week 3",
@@ -49,6 +54,40 @@ titles= [
     "Week 10",
     "Week 11",
 ]
+
+app = Flask(__name__)
+app.secret_key = 'secret key'
+
+# Load dotenv
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+def load_config(filename='database.ini', section='postgresql'):
+    parser = ConfigParser()
+    parser.read(filename)
+
+    # get section, default to postgresql
+    config = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            config[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+
+    return config
+# Connect to the PostgreSQL database
+def connect_db(config):
+    try:
+        # connecting to the PostgreSQL server
+        with psycopg2.connect(**config) as conn:
+            return conn
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
+
+# Dummy user database
+
+
 
 # Home route to redirect to login if not logged in
 @app.route('/')
@@ -67,7 +106,7 @@ def login():
         password = request.form.get('password')
 
         for user in users:
-            if username in (user["name"],user["username"]) and check_password_hash(user['password'], password):
+            if username in (user["name"], user["username"]) and check_password_hash(user['password'], password):
                 session["name"] = username
                 session['username'] = username  # Store the username in the session
                 return redirect(url_for('dashboard'))
@@ -81,77 +120,95 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('home'))
 
-#Dashboard route
+# Dashboard route
 @app.route('/dashboard')
 @app.route("/dashboard/<int:week>")
 def dashboard(week=0):
     users = get_users()
+    config=load_config()
+    conn = connect_db(config)
+    cur = conn.cursor()
+    if week == 0:
+        week = get_current_week() -1
 
     try:
-        with open('tmp/schedule.json') as f:
-            schedule = json.load(f)
+        cur.execute("SELECT schedule FROM schedule WHERE id = 1")
+        schedule = cur.fetchone()[0]
+        
     except:
-        schedule = {}
+        schedule = [{"date": date, "title": title, "availability": []} for date, title in zip(dates, titles)]
+    if not schedule:
+        schedule = [{"date": date, "title": title, "availability": []} for date, title in zip(dates, titles)]
 
     if 'username' in session:
-        name,user = get_user_and_name(users)
-        print(schedule[0])
+        name, user = get_user_and_name(users)
         return render_template('schedule.html', user=user, days=days, schedule=schedule[week], dates=dates, week=week, get_user=get_user)
-
     else:
         return redirect(url_for('login'))
 
-def get_user(name):
-    users = get_users()
-    return next((user for user in users if name in (user["name"],user["username"])), None)
-
-def get_users():
-    try:
-        with open('tmp/users.json') as f:
-            users = json.load(f)
-    except:
-        users = dummy_users
-
-    if not users:
-        users = dummy_users
-    return users
-
-
 @app.route('/schedule/<int:week>', methods=["GET", "POST", "PUT"])
-
 def schedule(week):
-    try:
-        with open('tmp/schedule.json') as f:
-            schedule = json.load(f)
-    except:
-        schedule = [{"date":date,"title":title, "availability":[]} for date,title in zip(dates,titles)]
-
     users = get_users()
+    config=load_config()
+    conn = connect_db(config)
+    cur = conn.cursor()
 
-    if request.method in ('POST',"PUT"):
-        availability = {}
-        for (day,time) in request.form.items(multi=True):
-            if day not in availability:
-                availability[day] = []
-            availability[day].append(time)
+    try:
+        cur.execute("SELECT schedule FROM schedule WHERE id = 1")
+        schedule = cur.fetchone()[0]
+    except:
+        schedule = [{"date": date, "title": title, "availability": []} for date, title in zip(dates, titles)]
+
+    if not schedule:
+        schedule = [{"date": date, "title": title, "availability": []} for date, title in zip(dates, titles)]
+
+    name, user = get_user_and_name(users)
+
+    availability = find_user_availability(schedule, week)
+    if request.method in ('POST', "PUT"):
+
         if 'username' in session:
-            current_week = schedule[week-1]
-            name, user = get_user_and_name(users)
-            current_week["availability"].append({name:availability})
-            with open('tmp/schedule.json', 'w') as f:
-                json.dump(schedule, f)
-            
+
+
+            current_week = schedule[week]
+
+            availability = {}
+            for (day, time) in request.form.items(multi=True):
+                if day not in availability:
+                    availability[day] = []
+                availability[day].append(time)
+
+            for person in current_week["availability"]:
+                if name in person:
+                    current_week["availability"].remove(person)
+                    break
+            current_week["availability"].append({name: availability})
+            cur.execute("UPDATE schedule SET schedule = %s WHERE id = 1", (json.dumps(schedule),))
+            conn.commit()
+
             return redirect(url_for('dashboard'))
         else:
             return redirect(url_for('login'))
 
-    return render_template('availability.html', days = list(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]),week=week, date=dates[week-1])
+    return render_template('availability.html', days=list(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]), week=week+1, date=dates[week], availability = availability)
+
+
+def find_user_availability(schedule, week):
+    for person in schedule[week]["availability"]:
+        if session["username"] in person:
+            return person[session["username"]]
+    return schedule[week]["availability"]
+
+
 
 # Update user data
-@app.route('/user', methods=["GET","POST",'PUT'])
+@app.route('/user', methods=["GET", "POST", 'PUT'])
 def user():
     # Creating a new user
     users = get_users()
+    config=load_config()
+    conn = connect_db(config)
+    cur = conn.cursor()
     if request.method == 'POST':
         name = request.form.get('displayName')
         username = request.form.get('username')
@@ -161,13 +218,12 @@ def user():
 
         delete_user = False
         pos = 0
-        for (index,user) in enumerate(users):
+        for (index, user) in enumerate(users):
             if user["name"] == name:
                 delete_user = True
                 pos = index
         if delete_user:
             users.pop(pos)
-
 
         users.append({
             "name": name,
@@ -175,31 +231,71 @@ def user():
             "password": generate_password_hash(password),
             "team": team,
             "colour": colour
-        }
-        )
+        })
 
-        with open('data/users.json', 'w') as f:
-            json.dump(users, f)
+        cur.execute("UPDATE users SET users = %s WHERE id = 1", (json.dumps(users),))
+
+        conn.commit()
 
         if session["username"] != "admin":
             session["username"] = username
+
+        conn.commit()
         return redirect(url_for('dashboard'))
 
     if request.method == 'GET':
         if 'username' in session:
-            name,user = get_user_and_name(users)
-            print(user)
+            name, user = get_user_and_name(users)
             return render_template('user.html', user=user, name=name, week=0)
 
     return redirect(url_for('login'))
 
-
 def get_user_and_name(users):
-    return next(((user["name"], user["username"]) for user in users if session['username'] in (user["name"],user["username"])), None)
+    return next(((user["name"], user["username"]) for user in users if session['username'] in (user["name"], user["username"])), None)
 
-def set_session(user):
-    session["name"] = user["name"]
-    session["username"] = user["username"]
+def get_users():
+    config=load_config()
+    conn = connect_db(config)
+    cur = conn.cursor()
+
+    cur.execute("SELECT users FROM users WHERE id = 1")
+    users = cur.fetchone()[0]
+    if not users:
+        users = dummy_users
+
+    conn.close()
+    return users
+
+def get_user(name):
+    users = get_users()
+    return next((user for user in users if name in (user["name"], user["username"])), None)
+
+def get_current_week():
+    today = datetime.date(2024, 2, 4)
+    for i, date in enumerate(dates):
+        if today < datetime.datetime.strptime(date, "%Y-%m-%d").date():
+            return i
+    return -1
 
 if __name__ == '__main__':
-    app.run()
+    config = load_config()
+    conn = connect_db(config)
+    if conn is not None:
+        cur = conn.cursor()
+
+        # Create tables if they don't exist
+        cur.execute("CREATE TABLE IF NOT EXISTS schedule (id SERIAL PRIMARY KEY, schedule JSONB)")
+        cur.execute("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, users JSONB)")
+
+        # Insert initial data if tables are empty
+        cur.execute("SELECT COUNT(*) FROM schedule")
+        if cur.fetchone()[0] == 0:
+            cur.execute("INSERT INTO schedule (id, schedule) VALUES (1, %s)", (json.dumps([]),))
+
+        cur.execute("SELECT COUNT(*) FROM users")
+        if cur.fetchone()[0] == 0:
+            cur.execute("INSERT INTO users (id, users) VALUES (1, %s)", (json.dumps(dummy_users),))
+
+        conn.commit()
+
+    app.run(debug=True)
