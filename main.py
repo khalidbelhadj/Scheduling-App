5c19e600d09835942679833b9c48355e1878854c
Bombearo <shekjaden@gmail.com>
2024-02-01 21:42:39 +0000

    Updated for env vars

--- main.py
+++ main.py
@@ -67,10 +67,18 @@ def load_config(filename='database.ini', section='postgresql'):
 
     # get section, default to postgresql
     config = {}
+    config['host'] = os.environ['POSTGRES_HOST']
+    config['database'] = os.environ['POSTGRES_DATABASE']
+    config['user'] = os.environ['POSTGRES_USER']
+    config['password'] = os.environ['POSTGRES_PASSWORD']
+
+    config["URL"] = os.environ["POSTGRES_URL"]
+
     if parser.has_section(section):
         params = parser.items(section)
         for param in params:
             config[param[0]] = param[1]
+
     else:
         raise Exception('Section {0} not found in the {1} file'.format(section, filename))
 
