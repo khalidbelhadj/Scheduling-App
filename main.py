3fcd26ef9375c6e98845dc7976c21a58a12d3c6e
Bombearo <shekjaden@gmail.com>
2024-02-01 21:38:06 +0000

    Updated requirements.txt again

--- main.py
+++ main.py
@@ -67,12 +67,12 @@ def load_config(filename='database.ini', section='postgresql'):
 
     # get section, default to postgresql
     config = {}
-    config["user"] = os.getenv("POSTGRES_USER")
-    config["password"] = os.getenv("POSTGRES_ASSWORD")
-    config["host"] = os.getenv("POSTGRES_HOST")
-    config["database"] = os.getenv("POSTGRES_DATABASE")
-
-    config["url"]=os.getenv("POSTGRES_URL")
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            config[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
 
     return config
 # Connect to the PostgreSQL database
